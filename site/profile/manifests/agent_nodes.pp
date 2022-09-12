d8cc2261ff32b3c3f5ce0d260a6aa386ce640c71
martindo99 <113306924+martindo99@users.noreply.github.com>
2022-09-12 19:48:30 -0300

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,7 +2,6 @@ class profile::agent_nodes {
   include dockeragent
     dockeragent::node {'web.puppet.vm':}
     dockeragent::node {'db.puppet.vm':}
-    
     host { ‘web.puppet.vm’ :
       ensure => present,
       ip     => ‘172.18.0.3’,
