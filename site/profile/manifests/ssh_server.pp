dc4678de7fa08265bfe30a0103ccda7c00d229b2
martindo99 <113306924+martindo99@users.noreply.github.com>
2022-09-12 18:43:25 -0300

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTH/JiKKbUbTugBqOhgGtwb+hSwZQB655uX1ahRQX7f33siOG1/kcTa+3+Q+9qzqE9Qro8Y9NWAJB0pBup1yHfoiAtY5x6IqyDYxg2Kr2mj2iGVDRRrHiainTlE0NXLwnc4xwEG1bvef5657Iv2EKBQRZloRVmXLfdZWN1cXdgVlZNyCutP9EU0CjTRiws4kf1Apap/PHZWlVYPOZHkoJv1j7UpQcfrpUtI+R9WksGzDsAYZ8fGavuKO0NEad1g2rtqpY/g5T2bHu7GIcPlXv/AsSnvOuxfzAUFAWvlFcvibLVvcKAa+mUbCKVUvpJ4P27fxYdEl7qtYp/BpulOr6j',
   }  
 }
